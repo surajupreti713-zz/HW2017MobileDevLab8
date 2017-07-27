7ee3ebc47504c25ad4a8725b3efda153e75902fe
diglesia <diglesia@google.com>
2017-07-27 16:56:21 -0700

    initial commit

---
+++ app/src/main/java/com/diglesia/hw2017mobiledev/lab7/MainActivity.java
@@ -0,0 +1,201 @@
+package com.diglesia.hw2017mobiledev.lab7;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "<your tag here>";
+    private static final int RC_SIGN_IN = 9001;
+
+    private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
+
+    private ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mProgressBar = (ProgressBar)findViewById(R.id.progress_bar);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        GoogleApiClient.OnConnectionFailedListener listener =
+                new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+                        // be available.
+                        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+                        Toast.makeText(MainActivity.this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+                        mProgressBar.setVisibility(View.INVISIBLE);
+                    }
+                };
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, listener)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_signin, menu);
+
+        // set menu item's title based on authentication state
+        MenuItem item = menu.findItem(R.id.action_account);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        item.setTitle(currentUser == null ? "sign in" : "sign out");
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_account) {
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            if (currentUser != null) { //user is signed in, so sign out
+                signOut();
+            } else {  //user is signed out, so sign in
+                signIn();
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                updateUI(null);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    // refresh the menu item, the text view, show a toast, and hide the spinner
+    // If user == null, then the user is logged out. If user != null, user is signed in.
+    private void updateUI(FirebaseUser user) {
+        invalidateOptionsMenu(); // This reloads the options menu items to display the correct string.
+        mProgressBar.setVisibility(View.INVISIBLE); // Hide the spinner.
+
+        // Show a Toast to the user.
+        String statusText = (user == null) ? "Signed out" : "Signed in as " + user.getEmail();
+        Toast.makeText(MainActivity.this, statusText , Toast.LENGTH_SHORT).show();
+
+        // Show or remove the MainFragment based on user signed-in state change.
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment currentFragment = fm.findFragmentById(R.id.container);
+        if (user != null && currentFragment == null) {
+            // If the user is signed IN, and there is NOT a fragment in the container, add it.
+            Fragment mainFragment = new MainFragment();
+            fm.beginTransaction().replace(R.id.container, mainFragment).commit();
+
+        } else if (user == null && currentFragment != null) {
+            // If the user is signed OUT, and there IS a fragment in the container, remove it.
+            fm.beginTransaction().remove(currentFragment).commit();
+        }
+    }
+
+    private void signIn() {
+        mProgressBar.setVisibility(View.VISIBLE);
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        mProgressBar.setVisibility(View.VISIBLE);
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        updateUI(null);
+                    }
+                });
+    }
+}
+
---
+++ app/src/main/java/com/diglesia/hw2017mobiledev/lab7/MainFragment.java
@@ -0,0 +1,25 @@
+package com.diglesia.hw2017mobiledev.lab7;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Get references to text views to display database data.
+        final TextView rootTextView = v.findViewById(R.id.root_text_view);
+        final TextView subTreeTextView = v.findViewById(R.id.subtree_text_view);
+
+        // Add database code here.
+
+        return v;
+    }
+
+}
