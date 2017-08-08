d26996088c6d95682e3c03615d675d09fb77f3a5
Suraj Upreti <surajupreti@Surajs-MacBook-Pro.local>
2017-08-07 20:42:10 -0700

    added databasecode

--- app/src/main/java/com/supreti/hw2017mobiledev/lab7/MainFragment.java
+++ app/src/main/java/com/supreti/hw2017mobiledev/lab7/MainFragment.java
@@ -7,6 +7,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainFragment extends Fragment {
 
     @Override
@@ -18,7 +24,37 @@ public class MainFragment extends Fragment {
         final TextView subTreeTextView = v.findViewById(R.id.subtree_text_view);
 
         // Add database code here.
-        
+        // set up reference to database root
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        // add a ValueEventListener on the ROOT of the database, to update the rootTextView.
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                rootTextView.setText(dataSnapshot.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                rootTextView.setText(databaseError.toString());
+            }
+        });
+
+        // Get a reference to "fun_path" element underneath the root.
+        DatabaseReference funPathReference = databaseReference.child("fun_path");
+        // add a ValueEventListener on that path of the database, to update subTreeTextView.
+        funPathReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                subTreeTextView.setText(dataSnapshot.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                subTreeTextView.setText(databaseError.toString());
+            }
+        });
+
 
         return v;
     }
